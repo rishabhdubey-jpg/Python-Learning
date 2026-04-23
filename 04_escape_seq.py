1dd23c1fb4cb752eaf2f36069df8906fee630c13
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-23 15:41:19 +0530

    Add files via upload

---
+++ 04_escape_seq.py
@@ -0,0 +1,15 @@
+a = "Rishabh is a good boy\nbut not \t a bad \"boy\""
+
+print(a)
+
+
+# ESCAPE SEQUENCE CHARACTERS
+
+# Sequence of characters after backslash "\" - Escape Sequence characters
+
+# Escape Sequence characters comprise of more than one character but represent one
+# character when used within the strings.
+
+# example  \n,  \t, l', \; \\ etc.
+
+#       newline tab singlequote backslash.

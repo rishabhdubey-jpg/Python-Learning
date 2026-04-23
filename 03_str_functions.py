a995bc7d3b744bfe846269276469fef02f903a07
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-23 15:41:06 +0530

    Add files via upload

---
+++ 03_str_functions.py
@@ -0,0 +1,14 @@
+name = "rishabh"
+name1 = "RISHABH Are"
+
+print(len(name))
+print(name.endswith("habh"))
+print(name.startswith("Ris"))
+print(name.capitalize())
+print(name1.capitalize()) # Return a capitalized version of the string.More specifically, make the first character have upper case and the rest lowercase.
+
+print(name1.casefold())  # Return a version of the string suitable for caseless comparisons. (all characters are in small letters)
+print(name1.swapcase())  # Convert uppercase characters to lowercase and lowercase characters to uppercase.
+
+print(name1.find("Are"))
+print(name.find("s"))

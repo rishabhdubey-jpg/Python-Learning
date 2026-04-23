4691ed168ba2dfb856efb3cac115cdd45c0f1cdd
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-23 15:40:51 +0530

    Add files via upload

---
+++ 02_negative_slicing.py
@@ -0,0 +1,10 @@
+name = "Rishabh"
+
+print(name[0:3])
+
+print(name[-4: -1])
+print(name[1:4])
+
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:7])
+print(name[1:7])

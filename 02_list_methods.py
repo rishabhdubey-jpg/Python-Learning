e3e6c0de3bf4c1c8f0c6dc67258cc86cf279fbc9
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-24 10:01:12 +0530

    Add files via upload

---
+++ 02_list_methods.py
@@ -0,0 +1,16 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Rishabh")  # Inseting an element "Rishabh" from the end of the list
+print(friends)
+
+l1 = [1, 34,62, 2, 6, 11]
+# l1.sort()            # It sorts the numbered list in ascending order.
+# l1.reverse()         # It reverses the order of the existing list.
+# l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3.
+# l1.remove(6)         # Will remove 6 from the list.
+# value = l1.pop(3)    # Will delete element at index 3 and return its value.
+# print(value)         # Prints the returend value at the index 3 of the list.
+
+# l1.clear()           # It clears the whole list.
+# l1.extend("Rishabh") # Divides a string in a single character for each word of the string.
+print(l1)

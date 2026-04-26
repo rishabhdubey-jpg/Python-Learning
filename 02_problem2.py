6f8ec964950910e85fe4526eebe4fb5b3491652f
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-26 23:51:51 +0530

    Add files via upload

--- 02_problem2.py
+++ 02_problem2.py
@@ -1,7 +1,25 @@
-# Write a program to fill in a letter template given below with name and date.
+# Write a program to accept marks of 6 students and display them in a sorted manner.
 
-letter = ''' Dear <|Name|>,
-You are selected!
-<|Date|> '''
+marks = []
 
-print(letter.replace("<|Name|>","Rishabh").replace("<|Date|>", "24 September 2050"))
+f1 = int(input("Enter Marks here: "))
+marks.append(f1)
+
+f2 = int(input("Enter Marks here: "))
+marks.append(f2)
+
+f3 = int(input("Enter Marks here: "))
+marks.append(f3)
+
+f4 = int(input("Enter Marks here: "))
+marks.append(f4)
+
+f5 = int(input("Enter Marks here: "))
+marks.append(f5)
+
+f6 = int(input("Enter Marks here: "))
+marks.append(f6)
+
+marks.sort()
+
+print(marks)

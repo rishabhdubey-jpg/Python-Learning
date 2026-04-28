b01ed566e70af010504b2fa30f5f98fac539758b
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-28 10:02:05 +0530

    Add files via upload

---
+++ 05_set_union_intersection.py
@@ -0,0 +1,5 @@
+s1 = {1, 45, 67, 32, 78}
+s2 = {33, 56, 79, 1, 78}
+
+print(s1.union(s2))         # Returns a new set with all items from both sets.
+print(s1.intersection(s2))  # Return a set which contains only item in both sets.

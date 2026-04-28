42733ba419a94e57ce8bde0eed824f614c3baf0f
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-28 10:01:43 +0530

    Add files via upload

---
+++ 04_set_methods.py
@@ -0,0 +1,16 @@
+s = {1, 5, 32, 54, 5, 5, "Rishabh"}
+print(s)
+
+# s.add(534)
+# print(s, type(s))
+
+# print(len(s))          # Returns the length of the set
+
+# s.remove(5)            # Updates the set s and removes 5 from s.
+# print(s, type(s))
+
+# print(s.pop())         # Removes an arbitrary element from the set and return the element removed.
+# print(s, type(s))
+
+# s.clear()              # Empties the set s.
+# print(s, type(s))

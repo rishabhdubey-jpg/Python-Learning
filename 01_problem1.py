cecf267ec9f82487ab624a43c6b3a9019bfafa53
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-29 10:20:43 +0530

    Add files via upload

--- 01_problem1.py
+++ 01_problem1.py
@@ -1,26 +1,12 @@
-# Write a program to store seven fruits in a list entered by the user.
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
 
-fruits = []
+words = {
+"madad": "Help",
+"kursi": "Chair",
+"billi": "Cat"
 
-f1 = input("Enter Fruit name: ")
-fruits.append(f1)
+}
 
-f2 = input("Enter Fruit name: ")
-fruits.append(f2)
+word = input("Enter the word you want meaning of: ")
 
-f3 = input("Enter Fruit name: ")
-fruits.append(f3)
-
-f4 = input("Enter Fruit name: ")
-fruits.append(f4)
-
-f5 = input("Enter Fruit name: ")
-fruits.append(f5)
-
-f6 = input("Enter Fruit name: ")
-fruits.append(f6)
-
-f7 = input("Enter Fruit name: ")
-fruits.append(f7)
-
-print(fruits)
+print(words[word])

b436e66890c357ff43ad35158943fa17005aa991
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-29 10:22:44 +0530

    Add files via upload

--- 02_problem2.py
+++ 02_problem2.py
@@ -1,25 +1,22 @@
-# Write a program to accept marks of 6 students and display them in a sorted manner.
+# Write a program to input eight numbers from the user and display all the unique numbers (once).
 
-marks = []
+s = set()
 
-f1 = int(input("Enter Marks here: "))
-marks.append(f1)
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+h = input("Enter number: ")
+s.add(int(n))
 
-f2 = int(input("Enter Marks here: "))
-marks.append(f2)
-
-f3 = int(input("Enter Marks here: "))
-marks.append(f3)
-
-f4 = int(input("Enter Marks here: "))
-marks.append(f4)
-
-f5 = int(input("Enter Marks here: "))
-marks.append(f5)
-
-f6 = int(input("Enter Marks here: "))
-marks.append(f6)
-
-marks.sort()
-
-print(marks)
+print(s)

fa24cbd6abafa9981d30aa93b89fc9c604240df8
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-04-29 10:25:49 +0530

    Add files via upload

--- 04_problem4.py
+++ 04_problem4.py
@@ -1,4 +1,17 @@
-# Write a program to sum a list with 4 numbers.
+# What will be the length of following set s:
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20') # length of s after these operations?
 
-l = [1, 5, 3, 2]
-print(sum(l))
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') 
+print(s)
+print("length =", len(s))
+
+# In Python, comparison operator " == " checks for equality between two values. When comparing
+# an integer ('1") and a floating-point number ('1.0"), Python evaluates their values and determines if
+# they are numerically equal.
+# ( Here integer 20 is equals to floating point number 20.0 )

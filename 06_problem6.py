1fea5e0504f7f3f857eef0e047ee86140f7b1040
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-01 11:39:26 +0530

    Add files via upload

--- 06_problem6.py
+++ 06_problem6.py
@@ -1,21 +1,31 @@
-# Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+# Write a program to calculate the grade of a student from his marks from the
+# following scheme:
+# 90-100 => Ex
+# 80-90  -> A
+# 70-80  -> B
+# 60-70  => C
+# 50-60  => D
+# <50    -> F
 
-d = {}
+marks = int(input("Enter the Marks (out of 100): "))
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
+if(marks >= 90 and marks <= 100):
+    print("Grade: Ex")
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
+elif(marks >= 80 and marks < 90):
+    print("Grade: A")
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
+elif(marks >= 70 and marks < 80):
+    print("Grade: B")
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
+elif(marks >= 60 and marks < 70):
+    print("Grade: C")
 
-print(d)
+elif(marks >= 50 and marks < 60):
+    print("Grade: D")
+
+elif(marks < 50 and marks >= 0):
+    print("Grade: F")
+
+else:
+    print("Invalid marks, Check again the entered marks..")

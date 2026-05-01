89043c2e20a9cb4a12d13a48af0dac290aeaca90
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-01 11:36:13 +0530

    Add files via upload

--- 05_problem5.py
+++ 05_problem5.py
@@ -1,5 +1,11 @@
-# s = {}
-# What is the type of 's'?
+# Write a program which finds out whether a given name is present in a list or not.
 
-s = {}
-print(type(s))
+l = ["Rishabh", "Bhavna", "Krishna", "Vansh", "Shristi", "Abhi"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")

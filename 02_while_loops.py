3d3097651eba29e9fe391c76661ff294191893cc
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-02 11:14:33 +0530

    Add files via upload

---
+++ 02_while_loops.py
@@ -0,0 +1,26 @@
+# WHILE LOOP
+
+# Syntax:
+
+# while (condition): # The block keeps executing until the condition is true
+#     #Body of the loop
+# In while loops, the condition is checked first. If it evaluates to true, the body of the loop
+# is executed otherwise not!
+
+# If the loop is entered, the process of [condition check & execution] is continued until
+# the condition becomes False.
+
+i = 1
+
+while(i<51):
+    print(i)
+    i +=1 # or i = i + 1
+
+# Output :
+# 1
+# 2
+
+# 3
+
+# 4
+# 5

381534516eb648fad2f1debf1474610d2f573603
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-05 10:04:06 +0530

    Add files via upload

--- 07_problem7.py
+++ 07_problem7.py
@@ -1,10 +1,14 @@
-# Write a program to find out whether a given post is talking about "Rishabh" or not.
+# Write a program to print the following star pattern.
+'''
 
-post = input("Enter the post: ")
+  *
+ ***
+***** for n = 3
 
-if("rishabh" in post.lower()):
-    print("This post is talking about rishabh")
+'''
 
-else:
-    print("This post is not talking about rishabh")
-
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    print(" "* (n-i), end="")    # (n-i) for the spaces before the star on every line, and (, end="") is for not taking a new line as from default print statement.
+    print("*"* (2*i-1), end="")  # (2*i-1) is a series of odd numbers.
+    print("")                    # For the new line.

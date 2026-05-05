430bb4be64abcd282939995cf6e18fc5ef532cd4
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-05 10:02:50 +0530

    Add files via upload

--- 02_problem2.py
+++ 02_problem2.py
@@ -1,16 +1,8 @@
-# Write a program to find out whether a student has passed or failed if it requires a
-# total of 40% and at least 33% in each subject to pass. Assume 3 subjects and
-# take marks as an input from the user.
+# Write a program to greet all the person names stored in a list 'l' and which starts with S.
+# l=["Harry", "Soham", "Sachin", "Rahul"]
 
-marks1 = int(input("Enter Marks 1: "))
-marks2 = int(input("Enter Marks 2: "))
-marks3 = int(input("Enter Marks 3: "))
+l = ["Harry", "Soham", "Sachin", "Rahul"]
 
-# Check for total percentage
-total_percentage = (100*(marks1+ marks2+ marks3))/300
-
-if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
-    print("You are passed:", total_percentage)
-
-else:
-    print("You failed, try again next year:", total_percentage)
+for name in l:
+    if(name.startswith("S")):
+        print(f"Hello {name}")

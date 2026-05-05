b9d0823c00602cbc2d17e19c345e3b27f6b24fa8
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-05 10:04:21 +0530

    Add files via upload

--- 08_problem8.py
+++ 08_problem8.py
@@ -1,23 +1,13 @@
-# If languages of two friends are same; what will happen to the program in problem 6?
+# Write a program to print the following star pattern.
+'''
 
-d = {}
+*
+**
+***     for n=3
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
+'''
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
 
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
-
-name = input("Enter friends name: ")
-lang = input("Enter Language name: ")
-d.update({name: lang})
-
-print(d)
-
-# In the dictionaries, a key is a identifier so it cannot me same, it will be updated, but values of any two keys can be same without any problem.
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    print("*"* (i)) 

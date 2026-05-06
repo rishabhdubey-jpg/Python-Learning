3e74f5ef36f3c8440393b430078dbae58ebad222
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-06 09:52:55 +0530

    Add files via upload

---
+++ 03_function_with_arguments.py
@@ -0,0 +1,14 @@
+def greet(name, ending):
+    print("Good Dayyy!, " + name)
+    print(ending)
+    return "DONE.."
+
+a = greet("Rishabh", "Thankyou")
+print(a)
+
+'''
+FUNCTIONS WITH ARGUMENTS
+
+A function can accept some value it can work with. We can put these values in the parentheses.
+
+'''

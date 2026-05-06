48a8e0bc41e1e49befb10eb04f0695271cab383a
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-06 09:53:08 +0530

    Add files via upload

---
+++ 04_function_with_arguments_example.py
@@ -0,0 +1,25 @@
+def avg(a, b, c):
+    average = (a + b + c)/3
+    # print(average)
+    return average
+
+def greet(gr):
+    print(gr)
+
+avg1 = avg(2, 4, 6)   # Function Call
+print(avg1)
+greet("Thank you!")
+
+avg2 = avg(22, 44, 66)
+print(avg2)
+greet("Thanks")
+
+avg3 = avg(345, 567, 789)
+print(avg3)
+greet("Donee")
+
+avg4 = avg(22, 0, -22)
+print(avg4)
+
+avg5 = avg(0, 453729, 646)
+print(avg5)

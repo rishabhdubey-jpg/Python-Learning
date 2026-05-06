ca5b73347cd4a2cf3f9e3bc85d46c06d64e53e85
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-06 09:52:18 +0530

    Add files via upload

---
+++ 01_functions.py
@@ -0,0 +1,57 @@
+# Function Definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: ") )
+    c = int(input("Enter your number: ") )
+
+    average = (a + b + c)/3
+    print(average)
+
+avg()   # Function Call
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
+
+'''
+
+A function is a group of statements performing a specific task,
+
+When a program gets bigger in size and its complexity grows, it gets difficult for a program to keep track on which piece of code is doing what!
+
+A function can be reused by the programmer in a given program any number of
+
+EXAMPLE AND SYNTAX OF A FUNCTION
+
+The syntax of a function looks as follows:
+
+def func1():
+    print("hello")
+This function can be called any number of times, anywhere in the program.
+
+FUNCTION CALL
+
+Whenever we want to call a function, we put the name of the function followed by parentheses as follows:
+
+func1() # This is called function call.
+
+FUNCTION DEFINITION
+
+The part containing the exact set of instructions which are executed during the function call.
+
+TYPES OF FUNCTIONS:
+There are two types of functions in python:
+
+. Built in functions (Already present in python)
+. User defined functions (Defined by the user)
+
+Examples of built in functions includes len(), print(), range() etc.
+
+The func1() function we defined is an example of user defined function.
+
+
+'''
+

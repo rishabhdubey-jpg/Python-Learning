27e0e6d9a116322a9f2b678fab3bb98a83d5150f
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-09 23:12:09 +0530

    Add files via upload

--- 02_problem2.py
+++ 02_problem2.py
@@ -1,8 +1,25 @@
-# Write a python program using function to convert Celsius to Fahrenheit.
+# The game() function in a program lets a user play a game and returns the score as an integer. 
+# You need to read a file 'Hi-score.txt' which is either blank or contains the previous Hi-score. 
+# You need to write a program to update the Hi-score whenever the game() function breaks the Hi-score.
+import random
 
-def c_to_f(c):
-    return (c * (9/5) + 32)
+def game():
+    print("You are playing the game..")
+    score = random.randint(1, 65)
+    # Fetch the hiscore
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore!=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+        
+    print(f"Your score is {score}")
+    if(score>hiscore):
+        # write this hiscore to the file
+        with open("hiscore.txt", "w") as f:
+            f.write(str(score))
+    
+    return score
 
-c = int(input("Enter temperature in °C: "))
-f = c_to_f(c)
-print(f"{round(f, 2)}F")
+game()

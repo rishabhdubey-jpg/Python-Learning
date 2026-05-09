064938daadce168676a5063311007fa4f56fabe3
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-09 23:12:25 +0530

    Add files via upload

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,6 +1,15 @@
-# How do you prevent a python print() function to print a new line at the end.
+# Write a program to generate multiplication tables from 2 to 20 and write it to the different files. 
+# Place these files in a folder for a 13 - year old.
 
-print("a")
-print("b")
-print("c", end="")
-print("d", end="")
+def generateTable(n):
+    table = ""
+    for i in range(1, 11):
+        table += f"{n} x {i} = {n*i}\n"
+    
+    with open(f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+
+for i in range(2, 21):
+    generateTable(i)
+

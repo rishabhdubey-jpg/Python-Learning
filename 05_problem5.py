bd5d777e1460f540f41ee518d429e7082deb57c2
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-13 15:22:11 +0530

    Add files via upload

--- 05_problem5.py
+++ 05_problem5.py
@@ -1,12 +1,20 @@
-# Repeat program 4 for a list of such words to be censored.
+# Write a class Train which has methods to book a ticket, get status (no of seats) and get fare information of train running under Indian Railways.
+from random import randint
 
-words = ["donkey", "leopard", "rohan"]
+class Train:
+    def __init__(self, trainNo):
+        self.trainNo = trainNo
+    
+    def book(self, fro, to):
+        print(f"Ticket is booked in train no: {self.trainNo} from {fro} to {to}")
 
-with open("file.txt", "r") as f:
-    content = f.read()
+    def getStatus(self):
+        print(f"Train no: {self.trainNo} is running on time")
 
-for word in words:
-    content = content.replace(word, "#" * len(word))
+    def getFare(self, fro, to):
+        print(f"Ticket fare in train no: {self.trainNo} from {fro} to {to} is: {randint(300, 1000)}")
 
-with open("file.txt", "w") as f:
-    f.write(content)
+t = Train(12398)
+t.book("Jabalpur", "Ujjain")
+t.getStatus()
+t.getFare("Jabalpur", "Ujjain")

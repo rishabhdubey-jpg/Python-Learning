0eb0ede97b666ad6b53fe03b3c99272d55a83d3b
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-13 15:22:26 +0530

    Add files via upload

--- 06_problem6.py
+++ 06_problem6.py
@@ -1,8 +1,22 @@
-# Write a program to mine a log file and find out whether it contains 'python'.
+# Can you change the self-parameter inside a class to something else (say *rishabh"). Try changing self to "slf" or "rishabh" and see the effects.
+from random import randint
 
-with open("log.txt") as f:
-    content = f.read()
-if("python" in content):
-    print("Yes, python is present")
-else:
-    print("No, python is not present")
+class Train:
+    def __init__(slf, trainNo):
+        slf.trainNo = trainNo
+    
+    def book(rishabh, fro, to):
+        print(f"Ticket is booked in train no: {rishabh.trainNo} from {fro} to {to}")
+
+    def getStatus(self):
+        print(f"Train no: {self.trainNo} is running on time")
+
+    def getFare(self, fro, to):
+        print(f"Ticket fare in train no: {self.trainNo} from {fro} to {to} is: {randint(300, 1000)}")
+
+t = Train(12398)
+t.book("Jabalpur", "Ujjain")
+t.getStatus()
+t.getFare("Jabalpur", "Ujjain")
+
+# There is no change in the output on changing the (self) to (slf) or (rishabh)

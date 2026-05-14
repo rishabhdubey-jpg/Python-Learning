84e5f9d94f307c1fb9367faf0ca0aef870c6ca2c
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-14 11:01:45 +0530

    Add files via upload

--- 04_problem4.py
+++ 04_problem4.py
@@ -1,26 +1,22 @@
-# Add a static method in problem 2, to greet the user with hello.
+# Write a class 'Complex' to represent complex numbers, along with overloaded operators '+' and '*' which adds and multiplies them.
 
-class calculator: 
-    def __init__(self, n):
-        self.n = n
-    
-    def square(self):
-        print(f"The square of {self.n} is {self.n*self.n}")
-    
-    def cube(self):
-        print(f"The cube of {self.n} is {self.n*self.n*self.n}")
-    
-    def sq_root(self):
-        print(f"The square root of {self.n} is {self.n**(1/2)}")
+class Complex:
+    def __init__(self, r, i):
+        self.r = r
+        self.i = i
 
-    @staticmethod
-    def greet():
-        print("Good morning..")
+    def __add__(self, c2):
+        return Complex(self.r + c2.r, self.i + c2.i)
 
-p = calculator(4)
-p.greet()
-p.square()
-p.greet()
-p.cube()
-p.greet()
-p.sq_root()
+    def __mul__(self, c2):
+        real_part = self.r * c2.r - self.i * c2.i   
+        imag_part = self.r * c2.i + self.i * c2.r
+        return Complex(real_part, imag_part)
+
+    def __str__(self):
+        return f"{self.r} + {self.i}i"
+
+c1 = Complex(1, 2)
+c2 = Complex(3, 4)
+print(c1+ c2)
+print(c1*c2)

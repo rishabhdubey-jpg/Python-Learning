dc1874b560d58e22f1630dd0a3d7c7c6cf6dbc05
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-14 11:02:48 +0530

    Add files via upload

--- 06_problem6.py
+++ 06_problem6.py
@@ -1,22 +1,31 @@
-# Can you change the self-parameter inside a class to something else (say *rishabh"). Try changing self to "slf" or "rishabh" and see the effects.
-from random import randint
+# Write __str__() method to print the vector as follows:
+# 7i + 8j +10k
+# Assume vector of dimension 3 for this problem.
 
-class Train:
-    def __init__(slf, trainNo):
-        slf.trainNo = trainNo
-    
-    def book(rishabh, fro, to):
-        print(f"Ticket is booked in train no: {rishabh.trainNo} from {fro} to {to}")
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-    def getStatus(self):
-        print(f"Train no: {self.trainNo} is running on time")
+    def __add__(self, other):
+        result = Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        return result
 
-    def getFare(self, fro, to):
-        print(f"Ticket fare in train no: {self.trainNo} from {fro} to {to} is: {randint(300, 1000)}")
+    def __mul__(self, other):
+        result = self.x * other.x + self.y * other.y + self.z * other.z
+        return result
 
-t = Train(12398)
-t.book("Jabalpur", "Ujjain")
-t.getStatus()
-t.getFare("Jabalpur", "Ujjain")
+    def __str__(self):
+        return f"{self.x}i + {self.y}j + {self.z}k"
 
-# There is no change in the output on changing the (self) to (slf) or (rishabh)
+# Test the implementation
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+v3 = Vector(7, 8, 9)  # Same dimension vector
+
+print(v1 + v2)  # Output: Vector(5, 7, 9)
+# print(v1 * v2)  # Output: 32
+
+print(v1 + v3)  # Output: Vector(8, 10, 12)
+# print(v1 * v3)  # Output: 50

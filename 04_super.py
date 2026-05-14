44c715bdd47d44055835368d596de683c5dcfe74
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-14 10:47:36 +0530

    Add files via upload

---
+++ 04_super.py
@@ -0,0 +1,33 @@
+class Employee:
+    def __init__(self):
+        print("Constructor of Employee")
+    a = 1
+
+
+
+class Programmer(Employee):
+    def __init__(self):
+        print("Constructor of Programmer")
+    b = 2
+
+
+class Manager(Programmer):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Manager")
+    c = 3
+
+
+# o = Employee()
+# print(o.a) # Prints the a attribute
+
+# o = Programmer()
+
+o = Manager()
+print(o.a, o.b, o.c) # Prints the a, b and c attributes
+
+# SUPER() METHOD
+# super() method is used to access the methods of a super class in the derived class.
+
+# super() ._ init_()
+# init () Calls constructor of the base class

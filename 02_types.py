e899224a9925e5e90fe9eeeeae1ea6ab1bdeaee7
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-16 12:37:04 +0530

    Add files via upload

---
+++ 02_types.py
@@ -0,0 +1,13 @@
+from typing import List, Union, Tuple
+
+# Variable type hint
+n : int = 5
+
+name: str = "Rishabh"
+
+# Function type hints
+def sum(a: int, b: int) -> int:
+    return a+b
+
+# TYPES DEFINITIONS IN PYTHON
+# Type hints are added using the colon (:) syntax for variables and the -> syntax for function return types.

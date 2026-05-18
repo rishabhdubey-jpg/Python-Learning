5d7c8a0ab712cbabec30ae21be469e17d7b4d42e
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-18 10:07:28 +0530

    Add files via upload

---
+++ 11_enumerate.py
@@ -0,0 +1,20 @@
+l = [3, 513, 53, 535]
+
+# index = 0
+# for item in l:
+# print(f"The item number at index {index} is {item}")
+# index += 1
+
+# This can be simplified using enumerate function
+
+for index, item in enumerate(l):
+    print(f"The item number at index {index} is {item}")
+
+'''
+ENUMERATE FUNCTION IN PYTHON
+The 'enumerate' function adds counter to an iterable and returns it
+
+for i,item in list1:
+print(i,item) # Prints the items of list 1 with index
+
+'''

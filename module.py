bd75477737a9581da6aab72546e8a3492081d57c
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-18 10:08:39 +0530

    Add files via upload

--- module.py
+++ module.py
@@ -1,2 +1,19 @@
-import flask
-install pip pyjokes
+def myFunc():
+    print("Hello world!")
+
+if __name__ == "__main__":
+        # If this code is directly executed-by running the file its present in
+        print("We are directly running this code")
+        myFunc()
+        print(__name__)
+
+'''
+IF __NAME__ == '__MAIN__' IN PYTHON
+
+'__name__' evaluates to the name of the module in python from where the program is ran.
+
+If the module is being run directly from the command line, the '__name__'is set to
+string"__main__". Thus, this behaviour is used to check whether the module is run
+directly or imported to another file.
+
+'''

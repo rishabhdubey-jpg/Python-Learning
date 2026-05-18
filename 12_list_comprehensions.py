ce2513c3ad97f99dc89a9932658b649cb5bb2a9d
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-18 10:07:45 +0530

    Add files via upload

---
+++ 12_list_comprehensions.py
@@ -0,0 +1,18 @@
+myList = [1, 2, 9, 5, 3, 5]
+
+# squaredList = []
+# for item in myList:
+#     squaredList.append(item*item)
+
+squaredList = [i*i for i in myList]
+
+print(squaredList)
+
+'''
+LIST COMPREHENSIONS
+List Comprehension is an elegant way to create lists based on existing lists.
+
+list1= [1,7,12,11,22,]
+list2 = [item for item in list 1 if item > 8]
+
+'''

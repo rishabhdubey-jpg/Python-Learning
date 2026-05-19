568cce3f5eac66fbb8a5d5a4a4cd60d1270c0d59
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-19 14:03:08 +0530

    Add files via upload

---
+++ 02_lambda.py
@@ -0,0 +1,19 @@
+# def square(n):
+#     return n*n
+
+square = lambda x: x*x
+print(square(5))
+
+'''
+Syntax:
+
+lambda arguments:expressions        # can be used as a normal function
+
+Example:
+
+square = lambda x:x*x
+square(6) # returns 36
+sum = lambda a,b,c:a+b+C
+sum(1,2,3) # returns 6
+
+'''

de7c13fdd90620c28905f2c3c73e72286a730c6d
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-19 14:03:25 +0530

    Add files via upload

---
+++ 03_join_method.py
@@ -0,0 +1,16 @@
+a = ["Rishabh", "Krishna", "Sourabh"]
+
+final = "_".join(a)
+print(final)
+
+'''
+JOIN METHOD (STRINGS)
+
+Creates a string from iterable objects.
+
+l = ["apple", "mango", "banana"]
+result = ", and, ".join(l)I
+print(result)
+The above line will return "apple,and,mango,and,banana".
+
+'''

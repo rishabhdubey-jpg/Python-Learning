09bb4a25c175d9f035741663180fd73402f1c301
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-19 14:01:14 +0530

    Add files via upload

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,21 +1,6 @@
-# Create a class 'Employee' and add salary and increment properties to it.
-# Write a method 'salaryAfterIncrement' method with a @property decorator with a setter
-# which changes the value of increment based on the salary.
+# Write a list comprehension to print a list which contains the multiplication table of a user entered number.
 
-class Employee:
-    salary = 234
-    increment = 20
+n = int(input("Enter a number: "))
 
-    @property
-    def salaryAfterIncrement(self):
-        return (self.salary + self.salary * (self.increment/100))
-
-    @salaryAfterIncrement.setter
-    def salaryAfterIncrement(self, salary):
-        self.increment = ((salary/self.salary) -1)*100
-
-e = Employee()
-# print(e.salaryAfterIncrement)
-e.salaryAfterIncrement = 280.8
-print(e.increment)
-
+table = [n*i for i in range(1, 11)]
+print(table)

ee39bf51fe4d214f4fb86cbbeb0929601772953e
RISHABH DUBEY <rishabhdx06@gmail.com>
2026-05-20 10:02:10 +0530

    Add files via upload

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,6 +1,6 @@
-# Write a list comprehension to print a list which contains the multiplication table of a user entered number.
+# A list contains the multiplication table of 7. write a program to convert it to vertical string of same numbers.
 
-n = int(input("Enter a number: "))
+table = [str(7*i) for i in range(1, 11)]
 
-table = [n*i for i in range(1, 11)]
-print(table)
+s = "\n".join(table)
+print(s)
